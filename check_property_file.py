d34a713a7b530631d2e723c9809cdc7e5ea7520a
Gabriel Birke <gb@birke-software.de>
2015-05-06 14:26:08 +0200

    Add command line arguments to check_property_file
    File names are no longer hard coded.
    Cleanup of script.

--- check_property_file.py
+++ check_property_file.py
@@ -6,21 +6,30 @@ existing keys, with line breaks/comments intact.
 """
 
 import linecheck
+import argparse
 
-read_vitro = open("vitro_all.properties", "r")
-read_all = open("all_de_DE.properties", "r")
-writefile = open("new.properties", "w")
+parser = argparse.ArgumentParser()
+parser.add_argument("original_file", help="Original all.properties file from Vitro or VIVO theme folder", metavar="ORIGINAL_FILE")
+parser.add_argument("translation_file", help="Properties file with translation from languages/themes/wilma/i18n folder", metavar="TRANSLATED_FILE")
+parser.add_argument("output_file", default="missing.properties", nargs='?', metavar="OUTPUT_FILE")
+args = parser.parse_args()
 
-dict_all = {}
-for dall in read_all:
-    dall = dall.strip().split('=')
-    if len(dall) == 1:
-        continue
-    dict_all[dall[0]] = dall[1]
+original = open(args.original_file, "r")
+translation = open(args.translation_file, "r")
+output_file = open(args.output_file, "w")
 
-# for testing
-# dict_all = {}
+def get_properties_dict(lines):
+    """ Get property => value dict from an iterable.
+        Comments, blank lines and multiline properties are ignored.
+    """
+    dict_all = {}
+    for line in lines:
+        items = line.strip().split('=', 1)
+        if len(items) < 2:
+            continue
+        dict_all[items[0]] = items[1]
+    return dict_all
 
-checker = linecheck.Checker(dict_all)
-writefile.write("".join(checker.get_lines(read_vitro)))
+checker = linecheck.Checker(get_properties_dict(translation))
+output_file.write("".join(checker.get_lines(original)))
 
